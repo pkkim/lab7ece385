90f6235d4754ed2a56dadd262c37db36152b2733
atuls <atulsyan850@gmail.com>
2024-11-21 18:14:47 -0600

    Added Vivado Project for the IP

---
+++ hdmi_text_controller_v2_0_project/hdmi_text_controller_v2_0_project.sim/sim_1/behav/xsim/hdmi_text_controller_tb_vlog.tcl
@@ -0,0 +1,5 @@
+# TCL script for Vivado project
+# Open project
+open_project C:/Users/atuls/lab7/lab7tmp/hdmi_text_controller_v2_0_project/hdmi_text_controller_v2_0_project.xpr
+# Run simulation
+run_simulation
